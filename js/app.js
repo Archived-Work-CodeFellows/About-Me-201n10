4132ac58d7ede1d464a7e372eff71a1cb2e89d9d
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-01-11 19:07:53 -0800

    Added the first user alert and console alerts

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var userPoints = 0;
+
+console.log('User has connected to the site');
+alert('Come on, come all to my guess game crawl');
+
+var user = prompt('I\'d like to know your name!', 'type your name');
+console.log('user will now be referred to as ' + user);
+
+alert('Welcome ' + user + '! I hope you are ready to play a game.');
+
