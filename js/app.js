4167bba73f0acc594fc01bfe53b074f5ea5a638d
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-01-12 18:46:21 -0800

    Added the functionality to question 2

--- js/app.js
+++ js/app.js
@@ -58,34 +58,50 @@ if(question1 === 'yes' || question1 === 'y') {
     console.log(user + ' points: ' + userPoints);
     console.log(user + ' attempts left: ' + userAttempts);
 }
-/*
-
-if(question1 === 'yes' || question1 === 'y') {
-    
-    alert('Hooray! You did it ' + user + '. I am in fact a musician! Originally I studied Classical Bassoon and Jazz Saxophone but now I just kinda play whatever I pick up when I need to do some sound design or composition! Enjoy your delicious point');
-    userPoints++;
-    console.log(user + ' now has ' + userPoints + ' points');
-
-} else {
-    
-    alert('Sorry you got this one wrong. I am a musician and originally studied Classical Bassoon and Jazz Saxophone. Now I just play whatever I pick up when I need to do sound design or compose. Hopefully you can guess better next time!');
-    console.log(user + ' has ' + userPoints + ' points');
-
-}
 
 alert('Alright lets move on to the next question! Here we go!');
-var question2 = prompt('Am I an incredibly geeky person? Yeah you probably don\'t know who I am so flip a coin. Coins are unbiased right?', 'type yes or no').toLowerCase();
-console.log(user +  ' has answered with ' + question2);
+while( userAttempts > 0) {
+
+   question2 = prompt('Am I an incredibly geeky person? Yeah you probably don\'t know who I am so flip a coin. Coins are unbiased right?', 'type yes or no').toLowerCase();
+   console.log(user +  ' has answered with ' + question2);
+
+    if(question2 === 'yes' || question2 === 'y') {
+        
+        break;
+    } else {
+        userAttempts--;
+        console.log(user + ' attempts left: ' + userAttempts);
+        switch(userAttempts) {
+            case 2:
+            alert('Hmm, try again '+ user + '!');
+            break;
+            case 1:
+            alert('Really...there are only two possible answers.');
+            break;
+            default:
+            alert('Lets just move on...');
+            break;
+        }
+    }
+}
 
 if(question2 === 'yes' || question2 === 'y') {
-    userPoints++;
+    
     alert('Yup you would be correct, or that coin was correct or however your preferred method of guessing...um...guessed. I come from a comic book family, playing board games, role playing, super heroes, sci-fi space western...you name it. It\'s a lot of fun to have an active imagination! Point for you!');
-    console.log(user + ' now has '+ userPoints + ' points');
+    userPoints++;
+    userAttempts = 3;
+    console.log(user + ' points: ' + userPoints);
+    console.log(user + ' attempts left: ' + userAttempts);
 } else {
 
+    userAttempts = 3;
+    alerts('attempts all gone, it\'s okay. I will give you more so you can play!');
     alert('hmm, that wasn\'t a super fair question. Plus it was out of the blue and a lot of pressure to just judge something pretty random. I\'m sorry. So get ready for another one!');
-    console.log(user + ' has ' + userPoints + ' points');
+    console.log(user + ' points: ' + userPoints);
+    console.log(user + ' attempts left: ' + userAttempts);
 }
+
+/*
 
 alert('Cool feeling good? Sweet.');
 var question3 = prompt('Do I have any siblings?', 'type yes or no').toLowerCase();
