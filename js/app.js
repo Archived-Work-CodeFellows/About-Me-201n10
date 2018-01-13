f61332d121dfadc67f2310a509f347456fd89809
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-01-12 18:34:26 -0800

    Changed functionality to include loops

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 var userPoints = 0;
 var userBonus = 0;
 var userAttempts = 3;
+var question1, question2, question3, question4, question5, bonusQuestion;
 
 
 console.log('User has connected to the site');
@@ -11,14 +12,15 @@ alert('Come on, come all to my guess game crawl');
 var user = prompt('I\'d like to know your name!', 'type your name');
 console.log('user will now be referred to as ' + user);
 console.log(user + ' currently has ' + userPoints + ' points');
+console.log(user + ' attempts left: ' + userAttempts);
 
 alert('Welcome ' + user + '! I hope you are ready to play a game.');
 alert('Here there will be a series of questions about me. Who am I? Well my name is Eric. Good luck ' + user + '!');
 
-var question1 = prompt("First Question: Am I a musician?", 'type yes or no').toLowerCase();
-console.log(user + ' has answered ' + question1);
+//var question1 = prompt("First Question: Am I a musician?", 'type yes or no').toLowerCase();
+//console.log(user + ' has answered ' + question1);
 
-/*while( userAttempts > 1) {
+while( userAttempts > 0) {
 
     question1 = prompt("First Question: Am I a musician?", 'type yes or no').toLowerCase();
     console.log(user + ' has answered ' + question1);
@@ -28,9 +30,35 @@ console.log(user + ' has answered ' + question1);
         break;
     } else {
         userAttempts--;
+        console.log(user + ' attempts left: ' + userAttempts);
+        switch(userAttempts) {
+            case 2:
+            alert('Hmm, try again '+ user + '!');
+            break;
+            case 1:
+            alert('Really...there are only two possible answers.');
+            break;
+            default:
+            alert('Lets just move on...');
+            break;
+        }
     }
-}*/
-//alert('you did it! wooooo!')
+}
+
+if(question1 === 'yes' || question1 === 'y') {
+
+    alert('Hooray! You did it ' + user + '. I am in fact a musician! Originally I studied Classical Bassoon and Jazz Saxophone but now I just kinda play whatever I pick up when I need to do some sound design or composition! Enjoy your delicious point');
+    userPoints++;
+    userAttempts = 3;
+    console.log(user + ' points: ' + userPoints);
+    console.log(user + ' attempts left: ' + userAttempts);
+} else {
+    alert('Sorry you got this one wrong. I am a musician and originally studied Classical Bassoon and Jazz Saxophone. Now I just play whatever I pick up when I need to do sound design or compose. Hopefully you can guess better next time!');
+    userAttempts = 3;
+    console.log(user + ' points: ' + userPoints);
+    console.log(user + ' attempts left: ' + userAttempts);
+}
+/*
 
 if(question1 === 'yes' || question1 === 'y') {
     
@@ -135,3 +163,4 @@ if(userPoints > 0) {
     alert('Oh no! I stole your points away! It\'s okay ' + user + '. You still win. Nobody loses here!');
 }
 console.log('end of the javascript stuff');
+*/
